368ac0dece85085581f694d9ccc2dc8b6de57d64
nikosamotkhe <niko34109@gmail.com>
2024-06-07 16:37:19 +0400

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='container'>
+        <div className="flex">
+            <div>
+                <h1>Navbar</h1>
+            </div>
+        </div>
+        <div>
+            <ul>
+                <li>
+                    <a href="#"></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
